486a1997a26d8c2dd1fcf8fd6764100eadaf68c6
MatsLuca <104196462+MatsLuca@users.noreply.github.com>
2026-01-09 18:37:26 +0100

    Initial Commit

---
+++ LatexTerminalLive/LatexTerminalLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatexTerminalLiveApp.swift
+//  LatexTerminalLive
+//
+//  Created by Mats-Luca Dagott on 09.01.26.
+//
+
+import SwiftUI
+
+@main
+struct LatexTerminalLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
